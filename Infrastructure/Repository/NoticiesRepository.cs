24264bcfd7bb15f6254449fdffd6edd4fc55f317
jardelmsa <jardelmorais@hotmail.com>
2024-06-28 17:18:36 -0300

    Criacao do metodos para add usuarios e limpeza de pastas e classes desnecessarias

--- Infrastructure/Repository/NoticiesRepository.cs
+++ Infrastructure/Repository/NoticiesRepository.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using Domain.Interfaces;
+using Entities.Entities;
+using Infrastructure.Configs;
+using Infrastructure.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Repository
 {
-   public class NoticiesRepository
+    public class NoticiesRepository : GenericRepository<Noticia>, INoticies
     {
-
+        private readonly DbContextOptions<Context> _optionsBuilder;
+        public NoticiesRepository()
+        {
+            _optionsBuilder = new DbContextOptions<Context>();
+        }
+        public async Task<List<Noticia>> ListarNoticias(Expression<Func<Noticia, bool>> exNoticia)
+        {
+           using(var banco = new Context(_optionsBuilder))
+            {
+                return await banco.Noticia.Where(exNoticia).AsNoTracking().ToListAsync();
+            }
+        }
     }
 }
---
+++ Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Interfaces;
+using Entities.Entities;
+using Infrastructure.Configs;
+using Infrastructure.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class UserRepository : GenericRepository<ApplicationUser>, IUser
+    {
+        private readonly DbContextOptions<Context> _optionsBuilder;
+
+        public UserRepository()
+        {
+            _optionsBuilder = new DbContextOptions<Context>();
+        }
+
+        public async Task<bool> AdicionarUsuario(string email, string senha, int idade, string celular)
+        {
+            try
+            {
+                using (var data = new Context(_optionsBuilder))
+                {
+                  await data.ApplicationUsers.AddAsync(
+                        new ApplicationUser
+                        {
+                            Email = email,
+                            PasswordHash = senha,
+                            Idade = idade,
+                            Celular = celular
+                        });
+
+                    await data.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+                
+            }
+            return true;
+        }
+    }
+}
